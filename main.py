75c6b02e33b8283d59f33ec0f2ff6f53ec271648
孙智辉 <sunzhihui6@huawei.com>
2026-07-11 22:56:27 +0800

    Reset: Clear all code, start fresh

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 """
-网络测试工具 - 主程序
+NetTest - Network Testing Tool
 """
 from kivy.app import App
 
 
 class NetTestApp(App):
     def build(self):
-        from main_screen import MainScreen
-        return MainScreen()
+        from kivy.uix.label import Label
+        return Label(text='NetTest', font_size='24sp')
 
 
 if __name__ == '__main__':
